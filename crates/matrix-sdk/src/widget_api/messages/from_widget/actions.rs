57d896e03da157be6ee46e684a60a68ff72a8417
Timo K <toger5@hotmail.de>
2023-07-20 18:39:25 +0200

    add more actions
    Signed-off-by: Timo K <toger5@hotmail.de>

--- crates/matrix-sdk/src/widget_api/messages/from_widget/actions.rs
+++ crates/matrix-sdk/src/widget_api/messages/from_widget/actions.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
 
 use url::Url;
@@ -13,6 +15,8 @@ pub enum FromWidgetAction {
     ContentLoaded(MessageBody<(), ()>),
     #[serde(rename = "org.matrix.msc2931.navigate")]
     Navigate(MessageBody<Url, Result<(), &'static str>>),
+    GetOpenId(MessageBody<(), GetOpenIdResponse>),
+    SendToDevice(MessageBody<SendToDeviceRequest, ()>)
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -22,4 +26,33 @@ pub struct SupportedVersions {
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ApiVersion {
     PreRelease,
+}
+
+// MSC1960
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum OpenIdState {
+    #[serde(rename = "allowed")]
+    Allowed,
+    #[serde(rename = "blocked")]
+    Blocked,
+    #[serde(rename = "request")]
+    PendingUserConfirmation,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct GetOpenIdResponse {
+    state: OpenIdState, //OpenIDRequestState;
+    original_request_id: String,
+    access_token: Option<String>,
+    expires_in: Option<i32>,
+    matrix_server_name: Option<String>,
+    token_type: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SendToDeviceRequest {
+    #[serde(rename = "type")]
+    message_type: String,
+    encrypted: bool,
+    content: HashMap<String, HashMap<String, serde_json::Value>>,
 }
--- crates/matrix-sdk/src/widget_api/messages/to_widget/actions.rs
+++ crates/matrix-sdk/src/widget_api/messages/to_widget/actions.rs
@@ -1,10 +1,25 @@
 use serde::{Deserialize, Serialize};
 
+use crate::widget_api::messages::message::ActionBody;
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(tag = "action")]
 pub enum ToWidgetAction {
     #[serde(rename = "capabilities")]
     SendMeCapabilities,
     #[serde(rename = "notify_capabilities")]
-    CapabilitiesUpdated
+    CapabilitiesUpdated,
+    #[serde(rename = "notify_capabilities")]
+    OpenIdCredentials,
+    #[serde(rename = "sent_to_device")]
+    SendToDevice(ActionBody<SendToDeviceRequestBody, ()>),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SendToDeviceRequestBody {
+    #[serde(rename = "type")]
+    message_type: String,
+    sender: String,
+    encrypted: bool,
+    messages: serde_json::Value,
 }
