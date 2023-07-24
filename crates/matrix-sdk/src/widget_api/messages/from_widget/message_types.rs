41449b9bb573ae949b4d22c5745d622f88659247
Timo K <toger5@hotmail.de>
2023-07-24 13:20:59 +0200

    Change from widget message to use Request again
    Signed-off-by: Timo K <toger5@hotmail.de>

--- crates/matrix-sdk/src/widget_api/messages/from_widget/message_types.rs
+++ crates/matrix-sdk/src/widget_api/messages/from_widget/message_types.rs
@@ -4,15 +4,17 @@ use url::Url;
 
 use crate::widget_api::messages::message::MessageBody;
 
+use super::Request;
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(tag = "action")]
 pub enum FromWidgetMessage {
     #[serde(rename = "supported_api_versions")]
-    GetSupportedApiVersion(MessageBody<(), SupportedVersions>),
+    GetSupportedApiVersion(Request<(), SupportedVersions>),
     #[serde(rename = "content_loaded")]
-    ContentLoaded(MessageBody<(), ()>),
+    ContentLoaded(Request<(), ()>),
     #[serde(rename = "org.matrix.msc2931.navigate")]
-    Navigate(MessageBody<Url, Result<(), &'static str>>),
+    Navigate(Request<Url, Result<(), &'static str>>),
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
