0d21692b086107f22eaf228523fa3ebdb44f71b1
Timo <16718859+toger5@users.noreply.github.com>
2023-08-04 20:22:57 +0200

    Update crates/matrix-sdk/src/widget_api/mod.rs
    Co-authored-by: Daniel Abramov <inetcrack2@gmail.com>

--- crates/matrix-sdk/src/widget_api/mod.rs
+++ crates/matrix-sdk/src/widget_api/mod.rs
@@ -1,9 +1,9 @@
 // TODO: Remove this supress once we're ready to write the documentation.
 #![allow(missing_docs)]
 
+pub mod driver;
 pub mod error;
 pub mod handler;
 pub mod messages;
-pub mod driver;
 
 pub use self::error::{Error, Result};
