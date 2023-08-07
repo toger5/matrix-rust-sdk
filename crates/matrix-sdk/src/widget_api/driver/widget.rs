63297e0feecdf065b31514369bb969640758705d
Timo K <toger5@hotmail.de>
2023-08-07 15:28:11 +0200

    more docs
    Signed-off-by: Timo K <toger5@hotmail.de>

--- crates/matrix-sdk/src/widget_api/driver/widget.rs
+++ crates/matrix-sdk/src/widget_api/driver/widget.rs
@@ -3,10 +3,11 @@ use async_trait::async_trait;
 use super::handler::Result;
 use crate::widget_api::messages::capabilities;
 
-// the Widget trait that needs to be implemented in the native client
+/// The Widget trait that needs to be implemented in the native client.
+/// An instance implementing this trait will be required to initialize the widget driver.
 #[async_trait]
 pub trait Widget {
-    /// This function will be called on the struct implementing the client widget whenever there is a new message available
+    /// This function will be called whenever there is a new message available
     /// that has to be sent to the widget.
     ///
     /// # Examples
@@ -20,26 +21,25 @@ pub trait Widget {
     ///```
     fn send_widget_message(&self, message: &str) -> Result<()>;
 
-    /// The client should show a dialog to approve all the capabilites that the widget requests.
-    /// The returned value than only contains the capability options the user has approved.
-    /// The client should also provide good phrasing for the filters:
+    /// The client should show a dialog to give the user the option approve some/all of the capabilites that the widget requests.
+    /// The returned value contains the capability options the user has approved.
+    /// The client should also provide good phrasing for the different permissions/filters:
     /// cap.send_room_event = [{event_type: "m.room.message", msgtype: "m.image" }] should be come sth like:
-    /// Allow the widget to:
-    /// Send images in this room.
+    /// Allow the widget to, send images in this room. (checkbox)
     async fn capability_permissions(
         &self,
         cap: capabilities::Options,
     ) -> Result<capabilities::Options>;
 
-    /// The client should show a dialog to approve if the widget is allowed to get an open id token.
-    /// A `OpenIdDialogResponse` is returned containing a flag if it was allowed and informing the widget driver
-    /// if the permission can be cached so the widget driver will not call `show_get_openid_dialog` again.
+    /// The client should show a dialog to approve if the widget is allowed to get an OpenId token.
+    /// A `OpenIdDialogResponse` is returned containing a flag if the user alloed the token request and
+    /// if the permission can be cached so the widget driver will not call `openid_permissions` on the widget again.
     async fn openid_permissions(&self) -> Result<OpenIdDialogResponse>;
 
-    /// Return the widget id from the widget state event.
+    /// Returns the widget id from the widget state event.
     fn id(&self) -> &str;
 
-    /// Return the widget state event as a raw string.
+    /// Returns the widget state event as a raw string.
     fn get_widget_state_json(&self) -> &str;
 }
 
