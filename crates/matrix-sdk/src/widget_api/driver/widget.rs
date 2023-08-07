c774b2316f90ce7d4d1c02d1bbd73c0bdc33500c
Timo <16718859+toger5@users.noreply.github.com>
2023-08-07 21:29:07 +0200

    Sender and listener in Capabilites (#16)
    * temp
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * move widget
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * check fixes
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * make it work with daniels help
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * fix by making async trait ?Send
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * merge sender and receiver into one
    We have state and room events now!!!
    Only to device missing but that is not mandatory for EC->EX
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * add event reader
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * add comment for future implementation of openId
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * remove to_device capability.
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * made events not private
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * Some cleanup
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * more cleanup od warning
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * add docs to widget trait
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * remove comment
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * add openid pseudocode
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * more docs
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * filter refactor
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    * fix typo
    
    Signed-off-by: Timo K <toger5@hotmail.de>
    
    ---------
    
    Signed-off-by: Timo K <toger5@hotmail.de>

--- crates/matrix-sdk/src/widget_api/driver/widget.rs
+++ crates/matrix-sdk/src/widget_api/driver/widget.rs
@@ -1,25 +1,71 @@
-use super::handler::Result;
-use crate::widget_api::messages;
+use async_trait::async_trait;
 
-// the Widget trait that needs to be implemented in the native client
+use super::handler::Result;
+use crate::widget_api::messages::capabilities;
+
+/// The Widget trait that needs to be implemented in the native client.
+/// An instance implementing this trait will be required to initialize the widget driver.
+#[async_trait]
 pub trait Widget {
-    /// This is the most important message to be implemented in the client driver
-    /// the function will be called on the struct implementing the client driver whenever there is a new message available
-    /// that has to be sent to the widget
+    /// This function will be called whenever there is a new message available
+    /// that has to be sent to the widget.
+    ///
+    /// # Examples
+    /// ```
+    /// implement Widget for MyWidget {
+    ///     fn send_widget_message(&self, message: &str) -> Result<()> {
+    ///         myWidgetIFrame.postmessage(message);
+    ///         Ok()
+    ///     }
+    /// }
+    ///```
     fn send_widget_message(&self, message: &str) -> Result<()>;
 
-    fn show_capability_request(&self, cap: messages::capabilities::Options);
+    /// The client should show a dialog to give the user the option approve some/all of the capabilites that the widget requests.
+    /// The returned value contains the capability options the user has approved.
+    /// The client should also provide good phrasing for the different permissions/filters:
+    /// cap.send_room_event = [{event_type: "m.room.message", msgtype: "m.image" }] should be come sth like:
+    /// Allow the widget to, send images in this room. (checkbox)
+    async fn capability_permissions(
+        &self,
+        cap: capabilities::Options,
+    ) -> Result<capabilities::Options>;
+
+    /// The client should show a dialog to approve if the widget is allowed to get an OpenId token.
+    /// A `OpenIdDialogResponse` is returned containing a flag if the user alloed the token request and
+    /// if the permission can be cached so the widget driver will not call `openid_permissions` on the widget again.
+    async fn openid_permissions(&self) -> Result<OpenIdDialogResponse>;
+
+    /// Returns the widget id from the widget state event.
     fn id(&self) -> &str;
+
+    /// Returns the widget state event as a raw string.
     fn get_widget_state_json(&self) -> &str;
 }
+
+#[derive(Debug)]
+pub struct OpenIdDialogResponse {
+    is_allowed: bool,
+    cache_permission: bool,
+}
+
+#[derive(Debug)]
 pub struct DummyWidget {}
+
+#[async_trait]
 impl Widget for DummyWidget {
-    fn send_widget_message(&self, message: &str) -> Result<()> {
+    fn send_widget_message(&self, _message: &str) -> Result<()> {
         todo!()
     }
 
-    fn show_capability_request(&self, cap: messages::capabilities::Options) {
-        todo!()
+    async fn capability_permissions(
+        &self,
+        cap: capabilities::Options,
+    ) -> Result<capabilities::Options> {
+        Ok(cap)
+    }
+    async fn openid_permissions(&self) -> Result<OpenIdDialogResponse> {
+        Ok(OpenIdDialogResponse { is_allowed: true, cache_permission: true })
     }
 
     fn id(&self) -> &str {
